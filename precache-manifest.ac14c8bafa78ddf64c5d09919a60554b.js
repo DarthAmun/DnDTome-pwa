398e6ddd509960adbc0ffeede6d50e0fcb216337
DarthAmun <37881139+DarthAmun@users.noreply.github.com>
2020-01-21 12:43:12 +0100

    Updates

---
+++ precache-manifest.ac14c8bafa78ddf64c5d09919a60554b.js
@@ -0,0 +1,29 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a31399ca5666671abf7462ec860d18c1",
+    "url": "/DnDTomePwa/index.html"
+  },
+  {
+    "url": "/DnDTomePwa/scripts/jsstore.worker.min.185ce60ce167155b0007fde8c75db4cf.js"
+  },
+  {
+    "revision": "775eb7b1eadabf1e112d",
+    "url": "/DnDTomePwa/static/css/main.9021ae36.chunk.css"
+  },
+  {
+    "revision": "c9d27444621faf7f2218",
+    "url": "/DnDTomePwa/static/js/2.fcf5c18c.chunk.js"
+  },
+  {
+    "revision": "775eb7b1eadabf1e112d",
+    "url": "/DnDTomePwa/static/js/main.20812762.chunk.js"
+  },
+  {
+    "revision": "512497cb8e4a91569015",
+    "url": "/DnDTomePwa/static/js/runtime-main.7e1a9e91.js"
+  },
+  {
+    "revision": "f65d1a07e0f4521c99d900e31e4bc530",
+    "url": "/DnDTomePwa/static/media/Quicksand-Medium.f65d1a07.ttf"
+  }
+]);
--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App_old from './App';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App_old />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
