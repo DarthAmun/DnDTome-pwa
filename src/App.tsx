0b72a659b49673cbec867f6efb1fbd15814b740b
DarthAmun <amon.loen@gmail.com>
2020-06-20 14:39:45 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+
+import * as React from "react";
+
+export interface HelloProps { compiler: string; framework: string; }
+
+// 'HelloProps' describes the shape of props.
+// State is never set so we use the '{}' type.
+export class App extends React.Component<HelloProps, {}> {
+    render() {
+        return <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>;
+    }
+}
