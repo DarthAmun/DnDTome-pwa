3040caa1082f12f519e6ecb4ca980e53a366c06a
DarthAmun <amon.loen@gmail.com>
2020-07-09 13:57:33 +0200

    searchbar styling

--- src/components/Spells/SpellOverview.tsx
+++ src/components/Spells/SpellOverview.tsx
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
-import { LoadingSpinner } from "../Loading";
-import Spell from "../../Data/Spell";
-import SpellTile from "./SpellTile";
-import AppWrapper from "../AppWrapper";
+import styled from "styled-components";
 import { MyAppDatabase } from "../../Database/MyDatabase";
 import { useTableByFilter } from "../../Hooks/DexieHooks";
 import Filter from "../../Data/Filter";
+import Spell from "../../Data/Spell";
+
+import { LoadingSpinner } from "../Loading";
+import SpellTile from "./SpellTile";
+import AppWrapper from "../AppWrapper";
 import SpellSearchBar from "./SpellSearchBar";
 
 const SpellOverview = () => {
@@ -16,15 +18,26 @@ const SpellOverview = () => {
   return (
     <AppWrapper>
       <SpellSearchBar onSend={(filterArray) => setFilter(filterArray)} />
-      {!error && loading && <LoadingSpinner />}
-      {!error &&
-        !loading &&
-        allSpells!.map((spell: Spell, index: number) => {
-          return <SpellTile key={index} spell={spell}></SpellTile>;
-        })}
-      {error && <>Fail</>}
+      <SpellContainer>
+        {!error && loading && <LoadingSpinner />}
+        {!error &&
+          !loading &&
+          allSpells!.map((spell: Spell, index: number) => {
+            return <SpellTile key={index} spell={spell}></SpellTile>;
+          })}
+        {error && <>Fail</>}
+      </SpellContainer>
     </AppWrapper>
   );
 };
 
 export default SpellOverview;
+
+const SpellContainer = styled.div`
+  margin-top: 50px;
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: flex-start;
+  align-content: flex-start;
+`;
--- src/components/Spells/SpellSearchBar.tsx
+++ src/components/Spells/SpellSearchBar.tsx
@@ -2,34 +2,79 @@ import React, { useState } from "react";
 import styled from "styled-components";
 import Filter from "../../Data/Filter";
 import ReactDOM from "react-dom";
+import StringField from "../FormElements/StringField";
+import NumberField from "../FormElements/NumberField";
+import CheckField from "../FormElements/CheckField";
+
+import {
+  faHourglassHalf,
+  faMortarPestle,
+  faHistory,
+  faPowerOff,
+  faUser,
+  faLink,
+  faBookOpen,
+  faSearch,
+  faRedoAlt,
+} from "@fortawesome/free-solid-svg-icons";
+import IconButton from "../FormElements/IconButton";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 interface $Props {
   onSend: (filters: Filter[]) => void;
 }
 
 const SpellSearchBar = ({ onSend }: $Props) => {
+  const [open, setOpen] = useState(false);
+
   const [name, setName] = useState<string>("");
-  // const [school, setSchool] = useState<string[]>([]);
-  // const [schoolList, setSchoolList] = useState<string[]>([]);
+  const [school, setSchool] = useState<string>("");
   const [level, setLevel] = useState<number>(-1);
-  // const [levelList, setLevelList] = useState<number[]>([]);
-  // const [ritual, setRitual] = useState<boolean>(false);
-  // const [time, setTime] = useState<string>("");
-  // const [range, setRange] = useState<string>("");
-  // const [duration, setDuration] = useState<string>("");
-  // const [components, setComponents] = useState<string>("");
-  // const [text, setText] = useState<string>("");
-  // const [classes, setClasses] = useState<string>("");
-  // const [sources, setSources] = useState<string>("");
+  const [ritual, setRitual] = useState<number>(0);
+  const [time, setTime] = useState<string>("");
+  const [range, setRange] = useState<string>("");
+  const [duration, setDuration] = useState<string>("");
+  const [components, setComponents] = useState<string>("");
+  const [text, setText] = useState<string>("");
+  const [classes, setClasses] = useState<string>("");
+  const [sources, setSources] = useState<string>("");
 
   const search = () => {
     let newFilters: Filter[] = [];
     if (name !== "") {
       newFilters = [...newFilters, new Filter("name", name)];
     }
+    if (school !== "") {
+      newFilters = [...newFilters, new Filter("school", school)];
+    }
+    if (time !== "") {
+      newFilters = [...newFilters, new Filter("time", time)];
+    }
+    if (range !== "") {
+      newFilters = [...newFilters, new Filter("range", range)];
+    }
+    if (duration !== "") {
+      newFilters = [...newFilters, new Filter("duration", duration)];
+    }
+    if (components !== "") {
+      newFilters = [...newFilters, new Filter("components", components)];
+    }
+    if (text !== "") {
+      newFilters = [...newFilters, new Filter("text", text)];
+    }
+    if (classes !== "") {
+      newFilters = [...newFilters, new Filter("classes", classes)];
+    }
+    if (sources !== "") {
+      newFilters = [...newFilters, new Filter("sources", sources)];
+    }
     if (level !== -1) {
       newFilters = [...newFilters, new Filter("level", level)];
     }
+    if (ritual) {
+      newFilters = [...newFilters, new Filter("ritual", ritual)];
+    }
+    setOpen(false);
     onSend(newFilters);
   };
 
@@ -37,38 +82,147 @@ const SpellSearchBar = ({ onSend }: $Props) => {
     ReactDOM.unstable_batchedUpdates(() => {
       setName("");
       setLevel(-1);
+      setSchool("");
+      setRitual(0);
+      setTime("");
+      setRange("");
+      setDuration("");
+      setComponents("");
+      setText("");
+      setClasses("");
+      setSources("");
+      setOpen(false);
     });
     onSend([]);
   };
 
   return (
-    <Bar>
-      <input
-        type="text"
-        name={name}
-        placeholder="Name..."
+    <Bar open={open}>
+      <StringField
         value={name}
-        onChange={(e) => setName(e.target.value)}
+        label="Name"
+        onChange={(name: string) => setName(name)}
       />
-      <input
-        type="number"
-        name={level + ""}
-        value={level}
-        onChange={(e) => setLevel(+e.target.value)}
+      <StringField
+        value={school}
+        label="School"
+        onChange={(school: string) => setSchool(school)}
       />
-      <button onClick={() => search()}>Search</button>
-      <button onClick={() => reset()}>Reset</button>
+      <FieldGroup>
+        <NumberField
+          value={level}
+          label="Level"
+          onChange={(level: number) => setLevel(level)}
+        />
+        <CheckField
+          value={!!ritual}
+          label="Ritual"
+          onChange={(ritual) => setRitual(ritual ? 1 : 0)}
+        />
+      </FieldGroup>
+      <StringField
+        value={time}
+        label="Time"
+        icon={faHistory}
+        onChange={(time) => setTime(time)}
+      />
+      <StringField
+        value={range}
+        label="Range"
+        icon={faPowerOff}
+        transform={{ rotate: 42 }}
+        onChange={(range) => setRange(range)}
+      />
+      <StringField
+        value={duration}
+        label="Duration"
+        icon={faHourglassHalf}
+        onChange={(duration) => setDuration(duration)}
+      />
+      <StringField
+        value={components}
+        label="Comp."
+        icon={faMortarPestle}
+        onChange={(components) => setComponents(components)}
+      />
+      <StringField
+        value={classes}
+        label="Classes"
+        icon={faUser}
+        onChange={(classes) => setClasses(classes)}
+      />
+      <StringField
+        value={sources}
+        label="Sources"
+        icon={faLink}
+        onChange={(sources) => setSources(sources)}
+      />
+      <StringField
+        value={text}
+        label="Text"
+        icon={faBookOpen}
+        onChange={(text) => setText(text)}
+      />
+      <IconButton onClick={() => search()} icon={faSearch} />
+      <IconButton onClick={() => reset()} icon={faRedoAlt} />
+
+      <SearchBarButton onClick={() => setOpen(!open)}>
+        <FontAwesomeIcon icon={faSearch} /> Search
+        </SearchBarButton>
     </Bar>
   );
 };
 
 export default SpellSearchBar;
 
-const Bar = styled.div`
+type SearchMode = {
+  open?: boolean;
+};
+
+const Bar = styled.div<SearchMode>`
+  position: absolute;
+  top: 4rem;
+  left: 0px;
+  z-index: 900;
+
+  transition: transform 0.3s ease-in-out;
+  transform: ${({ open }) => (open ? "translateY(0)" : "translateY(-100%)")};
+
   height: auto;
   min-height: 30px;
-  min-width: 100%;
+  min-width: calc(100% - 20px);
+  padding: 10px;
+  background: ${({ theme }) => theme.main.backgroundColor};
+  box-shadow: 0px 5px 5px 0px rgba(0, 0, 0, 0.75);
   flex: 1 1;
 
   display: flex;
+  flex-wrap: wrap;
+  align-items: flex-start;
+  align-content: flex-start;
+`;
+
+const SearchBarButton = styled.button`
+  position: absolute;
+  bottom: -50px;
+  left: calc(50% - 50px);
+
+  background-color: ${({ theme }) => theme.buttons.backgroundColor};
+  color: ${({ theme }) => theme.buttons.color};
+  box-shadow: 0px 5px 5px 0px rgba(0, 0, 0, 0.75);
+  border: none;
+  border-radius: 5px;
+  padding 10px;
+  box-sizing:content-box;
+  width: 80px;
+  height: 20px;
+  line-height: 20px;
+  cursor: pointer;
+`;
+
+const FieldGroup = styled.div`
+  flex: 2 1 auto;
+  display: flex;
+  flex-wrap: nowrap;
+  justify-content: space-around;
 `;
