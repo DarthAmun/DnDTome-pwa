ebd1581b9dee22ac6ad033c666889505c76ca4c2
DarthAmun <amon.loen@gmail.com>
2021-01-10 17:41:56 +0100

    Updates

---
+++ precache-manifest.ca90e518322ce29961d37e5380d6f70c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d08c2ddca5f11b50cf8859df1e4c0d4f",
+    "url": "/DnDTome-pwa/index.html"
+  },
+  {
+    "revision": "ccb3f17487642f2297ef",
+    "url": "/DnDTome-pwa/static/css/2.a4567992.chunk.css"
+  },
+  {
+    "revision": "1dc95deab54a289abe8d",
+    "url": "/DnDTome-pwa/static/css/main.a4aed14a.chunk.css"
+  },
+  {
+    "revision": "ccb3f17487642f2297ef",
+    "url": "/DnDTome-pwa/static/js/2.b4dd7f09.chunk.js"
+  },
+  {
+    "revision": "84d3aebfa55ae920a3ce92d6c6f557e2",
+    "url": "/DnDTome-pwa/static/js/2.b4dd7f09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1dc95deab54a289abe8d",
+    "url": "/DnDTome-pwa/static/js/main.5c9b623f.chunk.js"
+  },
+  {
+    "revision": "1f5dc9f02f21b62314c6",
+    "url": "/DnDTome-pwa/static/js/runtime-main.f1683600.js"
+  },
+  {
+    "revision": "a0b12edf02c3f325bdf7975e77ac2194",
+    "url": "/DnDTome-pwa/static/media/logo192.a0b12edf.png"
+  }
+]);
