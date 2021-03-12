3f7d8275e4b91f00e273146bf0045cb22ea50c12
DarthAmun <amon.loen@gmail.com>
2021-03-12 17:45:50 +0100

    Updates

---
+++ precache-manifest.e6b1fe70454675fb6a9e6395a2e29a1d.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b14eb24e9204535e1bfd830738adbc8d",
+    "url": "/DnDTome-pwa/index.html"
+  },
+  {
+    "revision": "8b018055b704aad37285",
+    "url": "/DnDTome-pwa/static/css/8.a4567992.chunk.css"
+  },
+  {
+    "revision": "b392c6ae1c716268acfe",
+    "url": "/DnDTome-pwa/static/css/main.a4aed14a.chunk.css"
+  },
+  {
+    "revision": "e39b952aa7fceac0cf7f",
+    "url": "/DnDTome-pwa/static/js/0.67bf8c0c.chunk.js"
+  },
+  {
+    "revision": "5107d80600d926a3f925",
+    "url": "/DnDTome-pwa/static/js/1.7a71f6b9.chunk.js"
+  },
+  {
+    "revision": "b9f6682423a777ac100d",
+    "url": "/DnDTome-pwa/static/js/10.0785b1ec.chunk.js"
+  },
+  {
+    "revision": "57f86820ffbd462d0e0adecadc603976",
+    "url": "/DnDTome-pwa/static/js/10.0785b1ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01040cddf298d867bc8e",
+    "url": "/DnDTome-pwa/static/js/11.f6717179.chunk.js"
+  },
+  {
+    "revision": "4174aaad4e66d2f40b7d44de5e40b86b",
+    "url": "/DnDTome-pwa/static/js/11.f6717179.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ac5a30fda0ef99f8635",
+    "url": "/DnDTome-pwa/static/js/12.6f2268b6.chunk.js"
+  },
+  {
+    "revision": "c36ca391af9a563550e3",
+    "url": "/DnDTome-pwa/static/js/13.5e283e2a.chunk.js"
+  },
+  {
+    "revision": "1714dd8d59e7d2a4fa42",
+    "url": "/DnDTome-pwa/static/js/14.8c772cd1.chunk.js"
+  },
+  {
+    "revision": "7ec92c30d093c202a92c",
+    "url": "/DnDTome-pwa/static/js/15.5aa7756a.chunk.js"
+  },
+  {
+    "revision": "60f93ce92c073c15cea3beb40867e872",
+    "url": "/DnDTome-pwa/static/js/15.5aa7756a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b95efcb7ff72c91cd4d5",
+    "url": "/DnDTome-pwa/static/js/16.4c0743d8.chunk.js"
+  },
+  {
+    "revision": "fb6926df737dadfd588e",
+    "url": "/DnDTome-pwa/static/js/17.1e3bb565.chunk.js"
+  },
+  {
+    "revision": "d40dc1d8781282c1ad8c",
+    "url": "/DnDTome-pwa/static/js/18.562dd92b.chunk.js"
+  },
+  {
+    "revision": "f59d6850691e7346a239",
+    "url": "/DnDTome-pwa/static/js/19.c66b43a5.chunk.js"
+  },
+  {
+    "revision": "5b6a3f1ca6e193597e4d49c7a42851aa",
+    "url": "/DnDTome-pwa/static/js/19.c66b43a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56178198ec558bb8472d",
+    "url": "/DnDTome-pwa/static/js/2.995add48.chunk.js"
+  },
+  {
+    "revision": "fc96e629811277c9deca",
+    "url": "/DnDTome-pwa/static/js/20.14e3dc15.chunk.js"
+  },
+  {
+    "revision": "e9b9c11ca666b67c5122",
+    "url": "/DnDTome-pwa/static/js/21.2b3ad584.chunk.js"
+  },
+  {
+    "revision": "2632d8b3cbbba0749e15",
+    "url": "/DnDTome-pwa/static/js/3.c256e32b.chunk.js"
+  },
+  {
+    "revision": "51f661889f1dbadc92ec",
+    "url": "/DnDTome-pwa/static/js/4.0c45790f.chunk.js"
+  },
+  {
+    "revision": "78267ad861eb13cb1537",
+    "url": "/DnDTome-pwa/static/js/5.7bbb5232.chunk.js"
+  },
+  {
+    "revision": "8b018055b704aad37285",
+    "url": "/DnDTome-pwa/static/js/8.00a83486.chunk.js"
+  },
+  {
+    "revision": "b79a4d2b5164bfc2101db15ee059d9a9",
+    "url": "/DnDTome-pwa/static/js/8.00a83486.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46ad8d5848bad28e4548",
+    "url": "/DnDTome-pwa/static/js/9.491b421b.chunk.js"
+  },
+  {
+    "revision": "57f86820ffbd462d0e0adecadc603976",
+    "url": "/DnDTome-pwa/static/js/9.491b421b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b392c6ae1c716268acfe",
+    "url": "/DnDTome-pwa/static/js/main.80ffdce9.chunk.js"
+  },
+  {
+    "revision": "d0b66832f63fc3a71d0b",
+    "url": "/DnDTome-pwa/static/js/runtime-main.674caacf.js"
+  },
+  {
+    "revision": "6b9dec78cb98ef9ec4d58ca64401c1d2",
+    "url": "/DnDTome-pwa/static/media/logo192.6b9dec78.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/DnDTome-pwa/precache-manifest.71f149048f34b760dd04826fe31faff5.js"
+  "/DnDTome-pwa/precache-manifest.e6b1fe70454675fb6a9e6395a2e29a1d.js"
 );
 
 self.addEventListener('message', (event) => {
