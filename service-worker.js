4643ffad19693921968c1adafc456daf55963b1a
DarthAmun <amon.loen@gmail.com>
2021-03-15 15:07:06 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/DnDTome-pwa/precache-manifest.95c322aaa690e99fda9b8eb1d0064861.js"
+  "/DnDTome-pwa/precache-manifest.48cb1fb48a548851e4029586f16c7703.js"
 );
 
 self.addEventListener('message', (event) => {
